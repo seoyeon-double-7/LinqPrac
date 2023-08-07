79f167d4997482715bcb6891bb96dbb2f6428a31
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-08-07 12:11:20 +0900

    Linq 사용해서 세련되게 값을 추출, 가공, 정렬해서 가져오기

--- Program.cs
+++ Program.cs
@@ -14,16 +14,23 @@ namespace LinqPrac
             List<int> output = new List<int>();
 
             // Linq 없이 2의 배수 구하기
-            foreach(var item in input)
+            foreach (var item in input)
             {
                 if (item % 2 == 0) output.Add(item);
             }
-
-            foreach (var item in input)
+            foreach (var item in output)
             {
-                Console.WriteLine(output);
+                Console.WriteLine(item);
             }
-            
+
+            var output2 = from item in input
+                          where item % 2 == 0
+                          orderby item descending
+                          select item * item;
+            foreach (var item in output2)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
